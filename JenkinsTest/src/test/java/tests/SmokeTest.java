2f164ec606107bb1aa9c8fbf5350c3bacf3e3e77
home <home@DESKTOP-HM6EP5A>
2026-01-03 21:55:59 +0530

    first

---
+++ JenkinsTest/src/test/java/tests/SmokeTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SmokeTest {
+
+	 protected WebDriver driver;
+	 
+    @Test
+    public void verifyGoogleTitle() {
+    	 WebDriverManager.chromedriver().setup();
+         driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Google"), "Title mismatch");
+
+        driver.quit();
+    }
+}
